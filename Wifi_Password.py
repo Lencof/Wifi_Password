75a870c8b6d00d450985c36a558ecc143b4e2cfa
Lencof <71154457+Lencof@users.noreply.github.com>
2021-01-28 18:57:04 +0300

    Add files via upload

---
+++ Wifi_Password.py
@@ -0,0 +1,15 @@
+# __author__ __Lencof__
+
+import random
+
+x="Telnet_130" # Your Wifi
+password="29121998" # Random Password
+while x!=password:
+    x=""
+    while len(x)!=len(password):
+        y=random.randrange(10)
+        x=str(x)+str(y)
+    print(x)
+print("Password:",x)
+
+# It took a lot of time 
