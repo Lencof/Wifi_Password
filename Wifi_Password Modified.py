e2288bd1f4aba0eaaeb13cde113986fd8523c9c5
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-14 06:28:31 +0300

    Update Wifi_Password Modified.py

--- Wifi_Password Modified.py
+++ Wifi_Password Modified.py
@@ -1,1 +1,15 @@
+# __Author__ __Lencof__
+# Wifi_Password Modified.py
 
+import random # use random
+
+x="Capo_125" # Your Wifi
+password="29121994598439058439058435894695846908549068895476895476896767548954906898" # Random Password
+while x!=password:
+  x=""
+  while len(x)!=len(password):
+      y=random.randrange(10)
+      x=str(x)+str(y)
+  print("Password:",x)
+  
+  # It took a lot of time
