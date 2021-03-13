db8f0784a53e5e53f350729f97a261a029ad660d
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-13 11:31:06 +0300

    Update Wifi_Password Modified №1.py

--- Wifi_Password Modified №1.py
+++ Wifi_Password Modified №1.py
@@ -1,1 +1,18 @@
+# __Author__ __Lencof__
+# Wifi_Password Modified №1.py
 
+import os # use os
+import sys # use sys
+import wifi # use wifi
+import random # use random
+
+x="Wifi" # your wifi
+password="12345678910" # your figures 
+while x!=password:
+  x=""
+  while len(x)!=len(password):
+    y=random.randrange(10)
+    x=str(x)+str(y)
+  print("Password:",x)
+  
+  # It took a lot of time
