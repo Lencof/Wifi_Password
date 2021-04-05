992dd560e4c1bb9eb93d86e3afcdbd3eb1f4e91e
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-05 04:33:23 +0300

    Update Wifi_Password Modified №4.py

--- Wifi_Password Modified №4.py
+++ Wifi_Password Modified №4.py
@@ -1,1 +1,25 @@
+# __Author__ __Lencof__
+# Wifi_Password Modified №4.py
 
+'''
+added new class changed numbers
+removed the sys module.
+'''
+
+import os
+import wifi
+import random
+
+# create class Wifi_Password():
+class Wifi_Password():
+    pass
+
+Wifi="Telnet_130" # Your Wifi
+password="1234567890" # Random Password
+while Wifi!=password:
+    Wifi=""
+    while len(Wifi)!=len(password):
+        y=random.randrange(10)
+        Wifi=str(Wifi)+str(y)
+    print(Wifi)
+print("Password:",Wifi)
