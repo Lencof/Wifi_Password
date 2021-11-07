74eca9a1dc7de0a65241c56fbf2058140e115191
Lencof <71154457+Lencof@users.noreply.github.com>
2021-11-07 18:39:23 +0600

    Update Wifi_Password Modified №2.py

--- Wifi_Password Modified №2.py
+++ Wifi_Password Modified №2.py
@@ -3,13 +3,13 @@
 
 import wifi
 
-x="Wifi" 
-password="dfbmkfdklbrftjhbiodfj940358231984mfdg" 
-while x!=password:
-  x=""
-  while len(x)!=len(password):
-    y=random.randrange(10)
-    x=str(x)+str(y)
+x = "Wifi" 
+password = "dfbmkfdklbrftjhbiodfj940358231984mfdg" 
+while x! = password:
+  x = ""
+  while len(x) != len(password):
+    y = random.randrange(10)
+    x = str(x) + str(y)
   print("Password:", x) 
   
   # It took a lot of time
